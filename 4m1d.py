d4241622d4ea480b23917f5b2a62a0e4f66f06f2
lyy8762 <lyy8762@qq.com>
2018-04-02 17:00:56 +0800

    4m2d：完善图片处理小程序，将图片以拍摄时间命名，若重名则加随机数，按年月创建文件夹分别保存。

--- 4m1d.py
+++ 4m1d.py
@@ -1,30 +1,37 @@
 import os
 import exifread
+import shutil
+import random
 
 
-def getExif(path,filename):
+def getExif(filename):
     FIELD = 'EXIF DateTimeOriginal'
     fd = open(filename, 'rb')
     tags = exifread.process_file(fd)
     fd.close()
     if FIELD in tags:
-        new_name = str(tags[FIELD]).replace(':', '').replace(' ', '_') + os.path.splitext(filename)[1]
-        tot = 1
-        while os.path.exists(new_name):
-            new_name = str(tags[FIELD]).replace(':', '').replace(' ', '_') + '_' + str(tot) + \
-                       os.path.splitext(filename)[1]
-            tot += 1
-
-        new_name2 = path+'\\'+new_name
-        print(new_name2)
-        os.rename(filename, new_name2)
+        newname = str(tags[FIELD]).replace(':', '').replace(' ', '_')
+        return newname+os.path.splitext(filename)[1]
     else:
         print('No {} found'.format(FIELD))
 
+def renamefile(path,file,newname):
+    while os.path.exists(os.path.join(path, newname)):
+        newname=os.path.splitext(newname)[0]+str(random.randint(0,9))+os.path.splitext(newname)[1]
+    os.rename(os.path.join(path, file), os.path.join(path, newname))
 
+def removefile(file,path,dir):
+    shutil.copyfile(path + '\\' + file, dir + '\\' + file)
 
 if __name__ == '__main__':
-    path='D:\\Projects\\python\\lyy\\pic'
+    #path='D:\\Projects\\python\\lyy\\pic'
+    path='C:\\Users\\lyy\\Desktop\\picture\\dir'
+
     for file in os.listdir(path):
         if os.path.isfile(os.path.join(path,file)):
-            getExif(path,os.path.join(path,file))
+            newname=getExif(os.path.join(path,file))
+            dir=os.path.join(path,newname[0:6])
+            if not os.path.isdir(dir):
+                os.makedirs(dir)
+            removefile(file,path,dir)
+            renamefile(dir,file,newname)
