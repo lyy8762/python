d41d1664f5722cb7afbc9336e3a0995edf782d86
liz0608 <liz0608@foxmail.com>
2018-04-03 23:00:14 +0800

    fundCheck:学习request模块之返回值

--- fundCheck.py
+++ fundCheck.py
@@ -1,15 +1,12 @@
-import urllib2
+import requests
 import re
 url = 'http://cfh.igoldenbeta.com/cfh-web/cfh/beta/list?market=0&sort=MZ'
 
-req=urllib2.Request(url)
-con=urllib2.urlopen(req)
+response=requests.get(url)
+print(response.status_code)
+print(type(response.text))
+print(response.text)
+print(response.cookies)
+print(type(response.content))
+print(response.content.decode("utf-8"))
 
-
-doc=con.read()
-#print doc.decode("GBK").encode("utf-8")
-
-r='dayreturn":"........'
-
-print re.findall(r,doc)
-#print doc
